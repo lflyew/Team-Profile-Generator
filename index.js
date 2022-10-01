69824c76ca67f33eebcb00b67d258883beeb24a0
Latoya Lyew <nursebae@Latoyas-MacBook-Pro.local>
2022-10-01 15:57:04 -0400

    creation of employee constructor

---
+++ index.js
@@ -0,0 +1,11 @@
+const inquirer = require('inquirer');
+
+const fs = require ('fs');
+
+const manager = require('');
+const engineer = require('');
+const intern = require('');
+
+const employees = [];
+
+
