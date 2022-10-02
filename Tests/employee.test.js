1eb9b0ea6f6e45b1f185ee71a3ca93bd65145b70
Latoya Lyew <nursebae@Latoyas-MacBook-Pro.local>
2022-10-01 20:26:35 -0400

    employee test script

---
+++ Tests/employee.test.js
@@ -0,0 +1,39 @@
+const employee = require('../lib/employee');
+
+//employee object created
+
+test ('creates an employee object', () => {
+    const employee = new employee('Hailey', 123, 'hailey@gmail.com' );
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number)); 
+    expect(employee.email).toEqual(expect.any(String)); 
+
+
+});
+
+//retrieve name from getname
+
+test('gets employee name', () => {
+    const employee = new employee ('Hailey', 123, 'hailey@gmail.com');
+    expect(employee.getname()).toEqual(expect.any(String));
+
+});
+
+//gets id from getID
+test('gets employee id', () => {
+    const employee = new employee ('Hailey', 123, 'hailey@gmail.com');
+    expect(employee.getid()).toEqual(expect.any(Number));
+
+});
+//gets email from getemail
+test('gets employee email', () => {
+    const employee = new employee ('Hailey', 123, 'hailey@gmail.com');
+    expect(employee.getemail()).toEqual(expect.stringContaining(employee.email.toString()));
+
+});
+//gets role from getrole
+test('gets employee role', () => {
+    const employee = new employee ('Hailey', 123, 'hailey@gmail.com');
+    expect(employee.getrole()).toEqual("employee");
+
+});
