38764fd752306e78366ad4be358bda37c7e69e93
Latoya Lyew <nursebae@Latoyas-MacBook-Pro.local>
2022-10-01 21:40:13 -0400

    engineer test

--- Tests/engineer.test.js
+++ Tests/engineer.test.js
@@ -1,1 +1,18 @@
 const engineer = require('../lib/engineer');
+
+//engineer object created
+test('creates an engineer object', () => {
+    const engineer = new engineer('Hailey', 123, 'hailey@gmail.com', 'hailey23');
+    expect(engineer.github) .toEqual(expect.any(String));
+});
+
+
+test('gets github value for engineer', () => {
+    const engineer = new engineer('Hailey', 123, 'hailey@gmail.com', 'hailey23');
+    expect(engineer.getgithub()) .toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+test('gets role of employee', () => {
+    const engineer = new engineer('Hailey', 123, 'hailey@gmail.com', 'hailey23');
+    expect(engineer.getrole()) .toEqual("engineer");
+});
